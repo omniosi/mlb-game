a451dc7e846dc7b048a3a8ffb64873a82cbf391e
ornette coleman <omniosi@gmail.com>
2016-01-15 13:42:29 -0800

    setup js for TweenLite animation

---
+++ build/js/main.js
@@ -0,0 +1,6 @@
+var bk = document.getElementsByClassName("bk")[0];
+var mutt = document.getElementsByClassName("mutt")[0];
+var odc = document.getElementsByClassName("odc")[0];
+var text = document.getElementsByTagName("h1")[0];
+
+// start TweenLite JS here
