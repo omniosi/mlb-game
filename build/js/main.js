a4fc5fe3e955bf3902d81a1edcdd1cafe0b72ad2
ornette coleman <omniosi@gmail.com>
2016-01-15 16:09:16 -0800

    greensock timeline trouble

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,22 @@
-var bk = document.getElementsByClassName("bk")[0];
-var mutt = document.getElementsByClassName("mutt")[0];
-var odc = document.getElementsByClassName("odc")[0];
-var text = document.getElementsByTagName("h1")[0];
+// var bk = document.getElementsByClassName("bk")[0];
+var bk = document.getElementById("bk");
+// var mutt = document.getElementsByClassName("mutt")[0];
+// var odc = document.getElementsByClassName("odc")[0];
+// var text = document.getElementsByTagName("h1")[0];
 
+window.onloud = function(){
 // start TweenLite JS here
+var tween = TweenLite.to(bk, 1, {
+// TweenLite.to("bk", 1, {
+	opacity:1,
+	ease: Power1.easeInOut
+	// onComplete: step2
+}, 1);
+var step2 = TweenLite.to(bk,1,{
+	// x:200,
+	// scale: 0.5,
+	// ease: Power1.easeInOut,
+	// onComplete: step3
+},3);
+// tween.play();
+}
