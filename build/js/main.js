46e8501d562fea4bb63258fb724c406529f12560
ornette coleman <omniosi@gmail.com>
2016-01-17 12:30:28 -0800

    finally got GSAP to work

--- build/js/main.js
+++ build/js/main.js
@@ -1,22 +1,23 @@
-// var bk = document.getElementsByClassName("bk")[0];
 var bk = document.getElementById("bk");
-// var mutt = document.getElementsByClassName("mutt")[0];
-// var odc = document.getElementsByClassName("odc")[0];
-// var text = document.getElementsByTagName("h1")[0];
+var mutt = document.getElementById("mutt");
+var odc = document.getElementById("odc");
+var text = document.getElementsByTagName("h1")[0];
 
-window.onloud = function(){
-// start TweenLite JS here
-var tween = TweenLite.to(bk, 1, {
-// TweenLite.to("bk", 1, {
-	opacity:1,
-	ease: Power1.easeInOut
-	// onComplete: step2
-}, 1);
-var step2 = TweenLite.to(bk,1,{
-	// x:200,
-	// scale: 0.5,
-	// ease: Power1.easeInOut,
-	// onComplete: step3
-},3);
-// tween.play();
+window.onload = function(){
+	var tween = TweenLite.to(bk, 1, {
+		opacity:1,
+		ease: Power1.easeInOut,
+		onComplete: step2
+	}, 1);
+	var step2 = TweenLite.to(mutt,1,{
+		opacity:1,
+		ease: Power1.easeInOut,
+		onComplete: step3
+	},2);
+	var step3 = TweenLite.to(mutt,1,{
+		// x:200,
+		scale: 0.5,
+		ease: Power1.easeInOut
+		// onComplete: step3
+	},3);
 }
