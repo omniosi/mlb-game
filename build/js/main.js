86eb53eb9a98fc3a2977893ecb87256453ed67af
ornette coleman <omniosi@gmail.com>
2016-01-20 15:07:14 -0800

    added src files

--- build/js/main.js
+++ build/js/main.js
@@ -3,6 +3,7 @@ var bk = document.getElementById("bk");
 var odc = document.getElementById("mutt");
 var odc = document.getElementById("odc");
 var text = document.getElementsByTagName("h1")[0];
+var plus = document.getElementById("plus");
 
 window.onload = function(){
 	var tween = TweenLite.to(bk, 1, {
@@ -13,34 +14,24 @@ window.onload = function(){
 	var step2 = TweenLite.to(mutt,1,{
 		opacity:1,
 		ease: Power1.easeInOut,
-		delay:1,
+		delay:2,
 		onComplete: step3
 	});
-	var step3 = TweenLite.to(mutt,0.5,{
-		x:-182,
-		y:-30,
-		scale: 0.5,
-		ease: Power1.easeInOut,
-		delay:2,
-		onComplete: step4
-	});
-	var step4 = TweenLite.to(odc,1,{
+	var step3 = TweenLite.to(odc,1,{
 		opacity:1,
-		ease: Power1.easeInOut,
-		delay:2,
-		onComplete: step5
-	});
-	var step5 = TweenLite.to(odc,0.5,{
-		x:-75,
-		y:60,
-		scale: 0.5,
 		ease: Power1.easeInOut,
 		delay:3,
-		onComplete: step6
+		onComplete: step4
 	});
-	var step6 = TweenLite.to(text,1,{
+	var step4 = TweenLite.to(plus,1,{
 		opacity:1,
 		ease: Power1.easeInOut,
-		delay:4
+		delay:4,
+		onComplete: step5
+	});
+	var step5 = TweenLite.to(text,1,{
+		opacity:1,
+		ease: Power1.easeInOut,
+		delay:5
 	});
 }
